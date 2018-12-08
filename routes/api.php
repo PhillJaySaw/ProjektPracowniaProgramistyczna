b674b14ae56319f811c0aa6aff2d4b9365a22931
philliplawniczak <philljaysaw@gmail.com>
2018-12-08 21:23:07 +0100

    created routes for articles

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,27 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', function () {
+    return response(['Article 1', 'Article 2', 'Article 3'], 200);
+});
+
+Route::get('articles/{article}', function ($articleId) {
+    return response()->json(['articleId' => "{$articleId}"], 200);
+});
+
+Route::post('articles', function() {
+    return response()->json([
+        'message' => 'Create success'
+    ], 201);
+});
+
+Route::put('articles/{article}', function() {
+    return response()->json([
+        'message' => 'Update success'
+    ], 200);
+});
+
+Route::delete('products/{product}', function() {
+    return response()->json(null, 204);
+});
